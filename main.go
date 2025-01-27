3705a4c37e9ea6c4fcf776cf26590fcd3126c450
sirdebar <semkochkin@gmail.com>
2025-01-27 23:20:50 +0300

    Initial commit

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 type Student struct {
+	Login    string
 	Name     string
 	Surname  string
 	Password string
@@ -15,17 +16,17 @@ type Student struct {
 type Teacher struct {
 	Login    string
 	Password string
-	Students []Students
+	Students []Student
 }
 
 func main() {
 	student := []Student{
-		{Name: "Jeremy", Surname: "Scott", Password: "123", Grades: []int{}},
-		{Name: "David", Surname: "Lin", Password: "321", Grades: []int{}},
+		{Login: "Jeremy1008", Name: "Jeremy", Surname: "Scott", Password: "123", Grades: []int{}},
+		{Login: "CoolDavid", Name: "David", Surname: "Lin", Password: "321", Grades: []int{}},
 	}
 	teacher := []Teacher{
-		{Login: "Delayla", Password: "T123", Students: []Students{}},
-		{Login: "Margaret", Password: "T123", Students: []Students{}},
+		{Login: "Delayla", Password: "T123", Students: []Student{}},
+		{Login: "Margaret", Password: "T123", Students: []Student{}},
 	}
 	fmt.Print("How do you want to log in?\n1. As Teacher\t 2. As Student\t 3. Exit\n")
 	var choice string
@@ -49,17 +50,48 @@ func main() {
 				foundTeacher = &teacher[i]
 				break
 			}
-
-			if foundTeacher == nil {
-				log.Fatal("Teacher not found")
-			}
-
-			if foundTeacher.Password != password {
-				log.Fatal("Wrong password")
-			}
-
-			fmt.Printf("Welcome back, %s", foundTeacher.Login)
 		}
+
+		// Проверяем, найден ли учитель (после цикла!)
+		if foundTeacher == nil {
+			log.Fatal("Teacher not found")
+		}
+
+		// Проверяем пароль
+		if foundTeacher.Password != password {
+			log.Fatal("Wrong password")
+		}
+
+		fmt.Printf("Welcome back, %s", foundTeacher.Login)
+		// Добавить меню
+
+	case "2":
+		fmt.Println("Type your Login:")
+		var slogin string
+		fmt.Scan(&slogin)
+
+		fmt.Print("Type your Password:\n")
+		var spassword string
+		fmt.Scan(&spassword)
+
+		var foundStudent *Student
+		for i := range student {
+			if student[i].Login == slogin {
+				foundStudent = &student[i]
+				break
+			}
+		}
+
+		if foundStudent == nil {
+			log.Fatal("Student not found") // Исправлено!
+		}
+
+		if foundStudent.Password != spassword {
+			log.Fatal("Wrong password")
+		}
+
+		fmt.Printf("Welcome back, %s %s", foundStudent.Name, foundStudent.Surname)
+		// Добавить меню
 
 	}
 
