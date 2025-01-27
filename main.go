15dea66d6a92f17e79aaada4dc057e8e61fd47fa
sirdebar <semkochkin@gmail.com>
2025-01-27 22:49:06 +0300

    Small fixes

--- main.go
+++ main.go
@@ -5,20 +5,28 @@ import (
 	"log"
 )
 
-type Students struct {
+type Student struct {
 	Name     string
 	Surname  string
 	Password string
 	Grades   []int
 }
 
-type Teachers struct {
+type Teacher struct {
 	Login    string
 	Password string
 	Students []Students
 }
 
 func main() {
+	student := []Student{
+		{Name: "Jeremy", Surname: "Scott", Password: "123", Grades: []int{}},
+		{Name: "David", Surname: "Lin", Password: "321", Grades: []int{}},
+	}
+	teacher := []Teacher{
+		{Login: "Delayla", Password: "T123", Students: []Students{}},
+		{Login: "Margaret", Password: "T123", Students: []Students{}},
+	}
 	fmt.Print("How do you want to log in?\n1. As Teacher\t 2. As Student\t 3. Exit\n")
 	var choice string
 	fmt.Scan(&choice)
@@ -27,6 +35,31 @@ func main() {
 	case "3":
 		log.Fatal("Exiting...")
 	case "1":
+		fmt.Print("Type your Login:\n")
+		var login string
+		fmt.Scan(&login)
+
+		fmt.Print("Type your Password:\n")
+		var password string
+		fmt.Scan(&password)
+
+		var foundTeacher *Teacher
+		for i := range teacher {
+			if teacher[i].Login == login {
+				foundTeacher = &teacher[i]
+				break
+			}
+
+			if foundTeacher == nil {
+				log.Fatal("Teacher not found")
+			}
+
+			if foundTeacher.Password != password {
+				log.Fatal("Wrong password")
+			}
+
+			fmt.Printf("Welcome back, %s", foundTeacher.Login)
+		}
 
 	}
 
